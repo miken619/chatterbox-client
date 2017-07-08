b27d9e0b6ec7668adbe69944c3cb2963cc8b8e57
Hack Reactor Student <student@hackreactor.com>
2017-07-07 18:49:01 -0700

    copied

--- client/env/config.example.js
+++ client/env/config.example.js
@@ -2,7 +2,9 @@
 // request that you make so you don't have to.
 
 // Put your parse application keys here!
+
+
 $.ajaxPrefilter(function (settings, _, jqXHR) {
-  jqXHR.setRequestHeader('X-Parse-Application-Id', 'PARSE_APP_ID');
-  jqXHR.setRequestHeader('X-Parse-REST-API-Key', 'PARSE_API_KEY');
+  jqXHR.setRequestHeader('X-Parse-Application-Id', 'hello');
+  jqXHR.setRequestHeader('X-Parse-REST-API-Key', 'world');
 });
